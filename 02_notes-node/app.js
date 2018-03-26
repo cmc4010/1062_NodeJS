53f397c29aa608cf57476abfa38e067d741504b4
Chao-Ming Chang <cmc4010@gmail.com>
2018-03-26 15:08:01 +0800

    short session #1

--- 02_notes-node/app.js
+++ 02_notes-node/app.js
@@ -1,6 +1,25 @@
 console.log('Starting app.js');
 
 const fs = require('fs'); // built-in module
-const _ = require('lodash'); // third party module
+const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js'); // custom js file
+
+const argv = yargs.argv;
+
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs', yargs.argv);
+
+if(command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list'){
+  notes.getAll();
+} else if (command === 'read'){
+  notes.getNote(argv.title);
+} else if (command === 'remove'){
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}
